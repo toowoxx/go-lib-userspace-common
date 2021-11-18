3e895bb1acb53250619c6905b61135f9cd642cda
Simão Gomes Viana <simao.gomes@toowoxx.de>
2021-11-18 10:32:41 +0100

    cmds: add dir+env+output variant

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -19,6 +19,17 @@ func RunCommandReturnOutput(path string, args ...string) ([]byte, error) {
 	return exec.Command(path, args...).Output()
 }
 
+func RunCommandInDirWithEnvReturnOutput(name string, dir string, env map[string]string, params ...string) ([]byte, error) {
+	cmd := exec.Command(name, params...)
+	if dir != "." {
+		cmd.Dir = dir
+	}
+	for key, value := range env {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", key, value))
+	}
+	return cmd.Output()
+}
+
 func RunCommandWithEnvReturnOutput(path string, env map[string]string, args ...string) ([]byte, error) {
 	cmd := exec.Command(path, args...)
 	for key, value := range env {
